63a917c84f0da45081a35540704b6eaf5bbe3922
durkes <durkes@users.noreply.github.com>
2023-05-24 13:04:03 -0500

    Make ProjectItem elements optional

--- src/components/projects/ProjectItem.js
+++ src/components/projects/ProjectItem.js
@@ -5,12 +5,12 @@ import { Box } from '@mui/material';
 function ProjectItem({ image, live, source, title, description }) {
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-         <Box component={'img'} src={image} alt={'mockup'} />
+         {image && <Box component={'img'} src={image} alt={'mockup'} />}
          <h1 style={{ fontSize: '2rem' }}>{title}</h1>
          <Box display={'flex'} flexDirection={'column'} gap={'0.5rem'}
             alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
-            <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'} />
-            <IconLink link={source} title={'Source Code'} icon={'fa fa-code'} />
+            {live && <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'} />}
+            {source && <IconLink link={source} title={'Source Code'} icon={'fa fa-code'} />}
          </Box>
       </Box>
    );

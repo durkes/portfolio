30fb24965116ae125f358e7dd47a6622dc8f5233
durkes <durkes@users.noreply.github.com>
2023-07-05 20:36:42 -0500

    update portfolio link style

--- src/components/projects/IconLink.js
+++ src/components/projects/IconLink.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import Style from './IconLink.module.scss';
+import classNames from 'classnames';
 import { Box } from '@mui/material';
 import ThemeContext from '../../context/ThemeContext';
 
@@ -9,8 +10,8 @@ function IconLink(props) {
    const { link, title, icon } = props;
    return (
       <a href={link} className={theme === 'dark' ? Style.iconlink_dark : Style.iconlink} target="_blank" rel="noopener noreferrer">
-         <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-            <i className={icon} /> {title}
+         <Box p={1} border={'2px solid black'} borderRadius={'25px'} padding={'0.5rem 0.9rem'}>
+            <i className={classNames(icon, Style.icon)} /> {title}
          </Box>
       </a>
    );

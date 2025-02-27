aa713d2b504cd79685ffa076164b685f5cf53b83
durkes <durkes@users.noreply.github.com>
2025-02-26 21:48:28 -0600

    add page transition to projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ProjectItem from './ProjectItem';
 import { Box, Grid } from '@mui/material';
 import { info } from '../../info/Info';
 
+const useImageLoader = (containerRef) => {
+    const [imagesLoaded, setImagesLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!containerRef.current) return;
+
+        const images = containerRef.current.getElementsByTagName('img');
+        const imagePromises = Array.from(images).map(img => {
+            return new Promise((resolve) => {
+                if (img.complete) {
+                    resolve();
+                } else {
+                    img.addEventListener('load', resolve);
+                    img.addEventListener('error', resolve); // Handle error cases too
+                }
+            });
+        });
+
+        Promise.all(imagePromises).then(() => setImagesLoaded(true));
+    }, [containerRef]);
+
+    return imagesLoaded;
+};
+
 export default function Projects() {
+    const containerRef = useRef(null);
+    const imagesLoaded = useImageLoader(containerRef);
+
     return (
-        <Box>
+        <Box ref={containerRef} sx={{ opacity: imagesLoaded ? 1 : 0, transition: 'opacity 0.2s ease' }}>
             <Grid container display={'flex'} justifyContent={'center'}>
                 {info.portfolio.map((project, index) => (
                     <Grid item xs={12} md={6} key={index}>

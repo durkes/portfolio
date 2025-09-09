2de102fe21ce89608e0a888e074a930e9787a5d6
durkes <durkes@users.noreply.github.com>
2025-09-09 18:26:04 -0500

    adj nav style

--- src/components/home/Toggler.js
+++ src/components/home/Toggler.js
@@ -5,7 +5,7 @@ export default function Toggler({ darkMode, handleToggleTheme }) {
    const transition = 'all 250ms ease';
 
    return (
-      <Box fontSize={'1.5rem'} sx={{ cursor: 'pointer', userSelect: 'none', ':hover': { transform: 'translateY(-3px)', transition: transition } }}>
+      <Box fontSize={'1.5rem'} sx={{ cursor: 'pointer', userSelect: 'none' }}>
          {
             darkMode ?
                <span onClick={handleToggleTheme} aria-label="Full Moon" role="img">🌕</span>
